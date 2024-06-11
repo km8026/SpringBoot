32b7e23e2eb390949d3459566d9506d19ef465f9
MinJunKim <167392999+km8026@users.noreply.github.com>
2024-06-11 10:49:29 +0900

    dd

---
+++ src/main/java/com/example/demo/Car.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car {
+  public void run() {
+    System.out.println("Car run!!!");
+  }
+}
---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class TestController {
+  @Autowired Car car;
+
+  @GetMapping("/test")
+  public String test(){
+    car.run();
+    return "<h1> 테스트 페이지</h1>";
+  }
+}
